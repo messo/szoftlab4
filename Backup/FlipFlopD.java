711b4fdca6feb82ca36762a179958f9eb9f44fa0
Dévényi Attila <devenyiat@gmail.com>
2011-04-21 16:19:22 +0200

    Merge branch 'master' of codaset.com:balintkrivan/override-csapat-projekt
    Conflicts:

---
+++ Backup/FlipFlopD.java
@@ -0,0 +1,56 @@
+package logsim.model.component.impl;
+
+import logsim.model.component.FlipFlop;
+
+/**
+ * D flipflop, mely felfutó órajelnél beírja a belsõ memóriába az adatbemeneten (D)
+ * lévõ értéket.
+ *
+ * @author gabooo
+ */
+public class FlipFlopD extends FlipFlop {
+
+    /**
+     * D bemenet lábának a száma.
+     */
+    private static final int D = 2;
+
+    public FlipFlopD(String name) {
+        super(name, 2);
+    }
+
+    /**
+     * Flipflop logika véglegesítésnél
+     */
+//    @Override
+//    protected void onCommit() {
+//        if (isActive()) {
+//            // bemenetén lévõ értéket beírjuk.
+//            outputs[0].setValue(in[0]);
+//        } else {
+//            // marad a régi.
+//            outputs[0].setValue(q);
+//        }
+//    }
+
+    @Override
+    public FlipFlopD copy(String newName) {
+        return new FlipFlopD(newName);
+    }
+
+    @Override
+    protected void storeInput() {
+        in[0] = getInputWire(2).getValue();
+    }
+
+    @Override
+    public void onEvaluation(){
+        if (isActive()) {
+            // bemenetén lévõ értéket beírjuk.
+            outputs[0].setValue(in[0]);
+        } else {
+            // marad a régi.
+            outputs[0].setValue(q);
+        }
+    }
+}
